3552f54a7f09122600d46939823f3f4b6852ffaf
Mattwmaster58 <mattwmaster58@gmail.com>
2021-01-04 21:37:18 -0500

    inline fn, add comment

--- githubQuickDelete.user.js
+++ githubQuickDelete.user.js
@@ -8,7 +8,11 @@
 // @updateURL    https://github.com/Mattwmaster58/github-quick-delete/raw/master/githubQuickDelete.user.js
 // ==/UserScript==
 
-let deleteButton = getDeleteButton();
+const nodes = document.querySelectorAll('.btn-danger[role="button"]');
+// we assume the last 'dangerous' button is the delete button...
+let deleteButton = nodes[nodes.length - 1];
+
+// ...but just to be sure we do a sanity check
 if (deleteButton.innerHTML.search('Delete this repository') === -1) {
     console.error('selected wrong delete button or the text has changed!', deleteButton)
 } else {
@@ -17,14 +21,11 @@ if (deleteButton.innerHTML.search('Delete this repository') === -1) {
     deleteButton.onclick = submitDeleteForm;
 }
 
-function getDeleteButton() {
-    const nodes = document.querySelectorAll('.btn-danger[role="button"]');
-    return nodes[nodes.length - 1];
-}
-
 function submitDeleteForm() {
     try {
+        // the form action URL will end with delete typically
         let deleteForm = document.querySelector('form[action$="delete"]')
+        // more implicit validation we have the right URL
         let repoName = deleteForm.action.match(/\.com\/(.*\/.*)\/settings\/delete/)[1];
         console.log(`repo name found: ${repoName}`);
         deleteForm.querySelector('input:not([type=hidden])').value = repoName;
